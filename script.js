d8eafbe3412cf9f7a3f58ac61e43f575297da076
Malin Persson <malin.persson02@hotmail.com>
2025-12-08 18:35:05 +0100

    Added header text and added an event listener to js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const key = '97b1d1a32c7021b4bfa47961f8635f86c825ebe1';
 
+const submit = document.querySelector('#submit');
+const audioElement = document.querySelector('audio');
+const genName = document.querySelector('#name');
+const scientificName = document.querySelector('#scientific');
+
 const getAudio = async (group, area) => {
   try {
     const url = `https://xeno-canto.org/api/3/recordings?query=q:">B"+area:${area}+grp:${group}&key=${key}`;
@@ -10,8 +15,8 @@ const getAudio = async (group, area) => {
     console.log(error)
   }
 };
-
-let group = 'birds';
-let area = 'Asia';
-
-getAudio(group, area);
+submit.addEventListener('click', function() {
+    let group = document.querySelector('#group').value;
+    let area = document.querySelector('#area').value;
+    getAudio(group, area);
+});
