26e6cd55dc00553f47adc6e1c984e6f1fe371a78
Malin Persson <malin.persson02@hotmail.com>
2025-12-08 18:43:28 +0100

    Added a rendom generator and dynamic info to be displayed

--- script.js
+++ script.js
@@ -11,11 +11,20 @@ const getAudio = async (group, area) => {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
+
+    const randomIndex = Math.floor(Math.random() * data.recordings.length);
+    audioElement.src = data.recordings[randomIndex].file;
+    genName.textContent = data.recordings[randomIndex].en;
+    scientificName.textContent = data.recordings[randomIndex].gen + ' ' + data.recordings[randomIndex].sp;
+
   } catch (error) {
     console.log(error)
   }
 };
-submit.addEventListener('click', function() {
+
+
+submit.addEventListener('click', function(event) {
+    event.preventDefault;
     let group = document.querySelector('#group').value;
     let area = document.querySelector('#area').value;
     getAudio(group, area);
